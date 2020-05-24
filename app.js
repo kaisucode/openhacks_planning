47a383ca89a18f9bde4cba4dff5f1a25979319f2
Alek Westover <alek.westover@gmail.com>
2020-05-23 21:37:26 -0400

    stuff

--- app.js
+++ app.js
@@ -12,62 +12,6 @@ app.get('/', function(req, res){
 	res.redirect("index.html");
 });
 
-function vec(x,y,z){
-  return {"x": x, "y": y, "z": z};
-}
-function sq (number) {
-  return number * number;
-}
-function vecMagSquared(v){
-  return sq(v.x) + sq(v.y) + sq(v.z);
-}
-function vecMag(v){
-  return Math.sqrt(vecMagSquared(v));
-}
-function vecDiff(a, b){
-  return {"x": a.x-b.x, "y": a.y-b.y, "z": a.z-b.z};
-}
-function addToVec(a, b){
-  a.x += b.x;
-  a.y += b.y;
-  a.z += b.z;
-}
-function multToVec(v, k){
-  v.x *= k;
-  v.y *= k;
-  v.z *= k;
-}
-
-function vecMult(v, k){
-  return {"x": v.x*k, "y": v.y*k, "z": v.z*k};
-}
-function normalizeVec(v){
-  return vecMult(v, 1/vecMagSquared(v));
-}
-function vecDiffMagSquared(a, b){
-  return vecMagSquared(vecDiff(a,b));
-}
-function vecToString(v){
-  return `${v.x.toFixed(3)}, ${v.y.toFixed(3)}, ${v.z.toFixed(3)}`;
-}
-
-
-// constants, exist in both files
-const MASS = {
-  "player": 1,
-  "booleits": 0.1,
-  "amoboxes": 1,
-  "extralife": 1
-};
-
-const RADIUS = {
-  "player": 0.5,
-  "booleits": 0.1,
-  "extralife": 1,
-  "amoboxes": 1
-}
-const PLAYERS = ["redA", "redB", "bluA", "bluB"];
-// constants, exist in both files
 
 let game_environment = {
   "redTeam": {
@@ -82,8 +26,8 @@ let game_environment = {
   "bluB": { "booleits": 10, "pos": {"x": 20, "y": 20, "z": 3}, "vel": vec(0,0,0), "onPlanet": false },
   "environment": {
     "asteroids": {
-      "0": {"pos": {"x": 1, "y": 400, "z": 3}, "vel": vec(0,0,0), "mass": 7, "r": 30},
-      "1": {"pos": {"x": 1, "y": 40, "z": 3}, "vel": vec(0,0,0), "mass": 7, "r": 30}
+      "0": {"pos": {"x": 1, "y": 400, "z": 3}, "vel": vec(0,0,0), "mass": 7, "r": 50},
+      "1": {"pos": {"x": 1, "y": 40, "z": 3}, "vel": vec(0,0,0), "mass": 7, "r": 50}
     },
     "amoboxes": {
       "0": {"pos": vec(1,2,100), "vel": vec(0,0,0)}
