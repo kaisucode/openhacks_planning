0f87956da1552474b90789b0646543ab9425e9a2
Alek Westover <alek.westover@gmail.com>
2020-05-24 00:12:07 -0400

    fix

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ io.sockets.on('connection', function(socket){
     let owner = action["owner"];
 
 		newBooleit = {
-			"pos": game_environment[owner].pos.slice(), 
+			"pos": {...game_environment[owner].pos}, 
 			"vel": action["vel"]
 		};
 
